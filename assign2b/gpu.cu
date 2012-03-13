a821c29c8344b0f81d43cd48787e6a12916ed983
Stephen Twigg <sdtwigg@eecs.berkeley.edu>
2012-03-12 22:54:40 -0700

    added common files and compute_forces_gpu(...)

--- assign2b/gpu.cu
+++ assign2b/gpu.cu
@@ -34,4 +34,108 @@ __global__ void distribute_gpu (microblock* mb_list, int mb_rows, int mb_cols, p
     }
 }
 
+__device__ void apply_force_gpu(particle_t &particle, particle_t &neighbor)
+{
+  double dx = neighbor.x - particle.x;
+  double dy = neighbor.y - particle.y;
+  double r2 = dx * dx + dy * dy;
+  if( r2 > cutoff*cutoff )
+      return;
+  //r2 = fmax( r2, min_r*min_r );
+  r2 = (r2 > min_r*min_r) ? r2 : min_r*min_r;
+  double r = sqrt( r2 );
+
+  //
+  //  very simple short-range repulsive force
+  //
+  double coef = ( 1 - cutoff / r ) / r2 / mass;
+  particle.ax += coef * dx;
+  particle.ay += coef * dy;
+
+}
+
+__global__ void compute_forces_gpu (microblock* mb_list, int mb_rows, int mb_cols, particle_t* particles)
+{
+    // Get X and Y co-ordinate of microblock
+    int mb_x = threadIdx.x + blockIdx.x * blockDim.x;
+    int mb_y = threadIdx.y + blockIdx.y * blockDim.y;
+    int thread_id = mb_y * mb_cols + mb_x;
+
+    // Make sure that we have a valid microblock to process
+    if((mb_x >= mb_cols) || (mb_y >= mb_rows))
+        return;
+		
+	int neighbors[8];
+	neighbors[p_sw] = ((mb_x != 0)             && (mb_y != 0)        ) ? (&microblocks[(mb_y-1)*mb_cols + (mb_x-1)]) : (NO_MB);
+	neighbors[p_s ] = (                           (mb_y != 0)        ) ? (&microblocks[(mb_y-1)*mb_cols + (mb_x  )]) : (NO_MB);
+	neighbors[p_se] = ((mb_x != mb_cols-1)     && (mb_y != 0)        ) ? (&microblocks[(mb_y-1)*mb_cols + (mb_x+1)]) : (NO_MB);
+
+	neighbors[p_w ] = ((mb_x != 0)                                   ) ? (&microblocks[(mb_y  )*mb_cols + (mb_x-1)]) : (NO_MB);
+	neighbors[p_e ] = ((mb_x != mb_cols-1)                           ) ? (&microblocks[(mb_y  )*mb_cols + (mb_x+1)]) : (NO_MB);
+
+	neighbors[p_nw] = ((mb_x != 0)             && (mb_y != mb_rows-1)) ? (&microblocks[(mb_y+1)*mb_cols + (mb_x-1)]) : (NO_MB);
+	neighbors[p_n ] = (                           (mb_y != mb_rows-1)) ? (&microblocks[(mb_y+1)*mb_cols + (mb_x  )]) : (NO_MB);
+	neighbors[p_ne] = ((mb_x != mb_cols-1)     && (mb_y != mb_rows-1)) ? (&microblocks[(mb_y+1)*mb_cols + (mb_x+1)]) : (NO_MB);
+	
+	for(int i = 0; i < mb_list[thread_id].n; ++i)
+	{
+		int cp = mb_list[thread_id].p_idx[i];
+		particles[cp].ax = particles[cp].ay = 0;
+		
+		// Collide with particles in my block
+		for(int j = 0; j < mb_list[thread_id].n; ++j)
+		{
+			if(i != j) // do not collide with self
+			{
+				apply_force_gpu(particles[cp], particles[mb_list[thread_id].p_idx[j]]);
+			}
+		}
+		
+		// Collide with particles in neighboring blocks
+		for(int k = 0; k < 8; ++k)
+		{
+			if(neighbors[k] != NO_MB) // Make sure I have a neighbor!
+			{
+				for(int j = 0; j < mb_list[neighbors[k]].n; ++j)
+				{
+					apply_force_gpu(particles[cp], particles[mb_list[neighbors[k]].p_idx[j]]);
+				}
+			}
+		}
+	}
+}
+
+__global__ void move_gpu (particle_t * particles, int n, double size)
+{
+
+  // Get thread (particle) ID
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  if(tid >= n) return;
+
+  particle_t * p = &particles[tid];
+    //
+    //  slightly simplified Velocity Verlet integration
+    //  conserves energy better than explicit Euler method
+    //
+    p->vx += p->ax * dt;
+    p->vy += p->ay * dt;
+    p->x  += p->vx * dt;
+    p->y  += p->vy * dt;
+
+    //
+    //  bounce from walls
+    //
+    while( p->x < 0 || p->x > size )
+    {
+        p->x  = p->x < 0 ? -(p->x) : 2*size-p->x;
+        p->vx = -(p->vx);
+    }
+    while( p->y < 0 || p->y > size )
+    {
+        p->y  = p->y < 0 ? -(p->y) : 2*size-p->y;
+        p->vy = -(p->vy);
+    }
+
+}
+
 
