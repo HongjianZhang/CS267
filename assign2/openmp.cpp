80ba7b09e059dd2acc87416b5540eec3270a3ae3
sdtwigg <sdtwigg@eecs.berkeley.edu>
2012-03-13 11:28:23 -0700

    bugfix for openmp in microblocks

--- assign2/openmp.cpp
+++ assign2/openmp.cpp
@@ -7,7 +7,6 @@
 #include "microblock.h"
 
 const double micro_length  = cutoff*2;
-const int default_mbuf_depth = 4;
 
 void mb_add_particle(microblock* microblock, particle_t* particle_addr);
 void mb_expand_particle(microblock* microblock, int new_max);
